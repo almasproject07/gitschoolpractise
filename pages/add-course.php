de543fc82554b64ad0f952acc950c126e959eedb
Rohit <rohit@cloudxchange.io>
2022-01-19 15:13:17 +0530

    changes in add-course.php - rohit

--- pages/add-course.php
+++ pages/add-course.php
@@ -28,7 +28,7 @@ if(isset($_POST['submit'])){
 <meta name="description" content="">
 <meta name="author" content="">
 
-<title>add course test11</title>
+<title>add course test2</title>
 
 <!-- Bootstrap Core CSS -->
 <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css"
